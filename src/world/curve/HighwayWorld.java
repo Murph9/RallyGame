5d4eff0a32cb0a961ac6ff310b97f499eafde358
Murph9 <murph9y@gmail.com>
2017-03-03 20:42:01 +1100

    Highway world now generates a few terrain pieces that join

--- src/world/curve/HighwayWorld.java
+++ src/world/curve/HighwayWorld.java
@@ -1,7 +1,13 @@
 package world.curve;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.imageio.ImageIO;
 
 import com.jme3.asset.AssetManager;
 import com.jme3.bullet.PhysicsSpace;
@@ -9,6 +15,7 @@ import com.jme3.bullet.control.RigidBodyControl;
 import com.jme3.collision.CollisionResult;
 import com.jme3.collision.CollisionResults;
 import com.jme3.material.Material;
+import com.jme3.material.RenderState.FaceCullMode;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.FastMath;
 import com.jme3.math.Matrix3f;
@@ -56,9 +63,12 @@ public class HighwayWorld implements World {
 	
 	private Roads road;
 	
+	private int terrainSize;
+	
 	public HighwayWorld() {
 		rootNode = new Node("curveWorldRoot");
 		road = new Roads();
+		terrainSize = 16;
 	}
 	
 	@Override
@@ -78,21 +88,24 @@ public class HighwayWorld implements World {
 		
 		AssetManager am = App.rally.getAssetManager();
 		
-		generateTerrain(am, view);
-		generateRoads(am, view);
+		generateTerrain(am, view, 4);
+//		generateRoads(am, view);
 		
-		fixTerrainHeights();
+//		fixTerrainHeights();
+		
+//		saveTerrainToFile();
 		
 		return rootNode;
 	}
 	
-	private void generateTerrain(AssetManager am, ViewPort view) {
+	private void generateTerrain(AssetManager am, ViewPort view, int count) {
 		//create the terrain (from terrain world)
 		Material terrainMaterial = new Material(am, "Common/MatDefs/Terrain/HeightBasedTerrain.j3md");
+		terrainMaterial.getAdditionalRenderState().setFaceCullMode(FaceCullMode.Off);
 		
-		float grassScale = 64; //all 16
-        float dirtScale = 64;
-        float rockScale = 64;
+		float grassScale = 16; //all 16
+        float dirtScale = 16;
+        float rockScale = 16;
 		
         // GRASS texture
         Texture grass = am.loadTexture("Textures/Terrain/splat/grass.jpg");
@@ -121,49 +134,53 @@ public class HighwayWorld implements World {
         terrainMaterial.setTexture("slopeColorMap", rock2);
         terrainMaterial.setFloat("slopeTileFactor", 32); //32
 
-		//for reference some other implementations:
-//		Texture heightMapImage = am.loadTexture("Textures/Terrain/splat/mountains512.png");
-//		map = new ImageBasedHeightMap(heightMapImage.getImage());
-//		map = new HillHeightMap(1025, 1000, 50, 200, FastMath.rand.nextLong());
-		
-		map = new DiamondSquareMap(1025);
-	    map.load();
-	    
-	    map.normalizeTerrain(100); //TODO affects the terrain textures
-	    
-	    terrain = new TerrainQuad("my terrain", 65, 1025, map.getHeightMap());
-	    terrain.setMaterial(terrainMaterial);
-	    terrain.setShadowMode(ShadowMode.CastAndReceive);
-	    terrain.setLocalScale(2.5f, 1, 2.5f); //its 1:1 without this (terrain point per meter)
-	    
-	    Material m_debug = new Material(am, "Common/MatDefs/Misc/Unshaded.j3md");
-	    m_debug.setColor("Color", ColorRGBA.Black);
-	    
-	    rootNode.attachChild(terrain);
-	    List<Camera> cameras = new ArrayList<Camera>();
-	    cameras.add(view.getCamera());
-	    
-	    TerrainLodControl control = new TerrainLodControl(terrain, cameras);
-	    terrain.addControl(control);
-	    c = new RigidBodyControl(0);
-
-	    terrain.addControl(c);
-	    phys.add(c);
-	    //end terrain
+        for (int i = 0; i < count; i++) {
+        	Vector2f location = new Vector2f(0, i*terrainSize);
+        	
+			PerlinNoise pNoise = new PerlinNoise(terrainSize + 1, 2, 0.1f, 2, 2);
+			pNoise.SetOrigin(location);
+			pNoise.load();
+		    
+			pNoise.normalizeTerrain(255); //scale the values from diamondsquaremap to 255 
+		    
+			TerrainQuad terrain = new TerrainQuad("my terrain", 1025, terrainSize + 1, pNoise.getHeightMap());
+		    terrain.setMaterial(terrainMaterial);
+		    terrain.setShadowMode(ShadowMode.CastAndReceive);
+		    terrain.setLocalTranslation(location.x*2, 0, location.y*2); //by global scale
+		    terrain.setLocalScale(2, 4/255f, 2); //1 means 1:1 world unit
+		    
+		    Material m_debug = new Material(am, "Common/MatDefs/Misc/Unshaded.j3md");
+		    m_debug.setColor("Color", ColorRGBA.Black);
+		    
+		    rootNode.attachChild(terrain);
+		    List<Camera> cameras = new ArrayList<Camera>();
+		    cameras.add(view.getCamera());
+		    
+		    TerrainLodControl control = new TerrainLodControl(terrain, cameras);
+		    terrain.addControl(control);
+		    RigidBodyControl c = new RigidBodyControl(0);
+	
+		    terrain.addControl(c);
+		    phys.add(c);
+		    //end terrain
+        }
 	}
 
 	private void generateRoads(AssetManager am, ViewPort view) {
 		float height = terrain.getHeight(new Vector2f(0,0.001f));
+		height = (Float.isNaN(height) ? 0 : height);
 		float height2 = terrain.getHeight(new Vector2f(0,75));
+		height2 = (Float.isNaN(height2) ? height : height2);
 		Vector3f[] points = new Vector3f[] {
 				new Vector3f(0,height,0),
 				new Vector3f(0,height,50),
 				new Vector3f(0,height2,25),
 				new Vector3f(0,height2,75),
 			};
+		H.p("road start height: " + height + ", world start height:" + this.getWorldStart().y);
 		Curve curve = null;
-		curve = new StraightCurve(new Vector3f[] { points[0],points[3] }, Roads.CurveFunction());
-//		curve = new BeizerCurve(points, Roads.curveFunction());
+//		curve = new StraightCurve(new Vector3f[] { points[0],points[3] }, Roads.CurveFunction());
+		curve = new BeizerCurve(points, Roads.CurveFunction());
 		road.placePiece(new CurveQueueObj(0, curve, ""), rootNode, phys, true);
 		
 		for (int i = 0; i < 10; i++) {
@@ -184,8 +201,8 @@ public class HighwayWorld implements World {
 			points[2] = points[0].add(new Vector3f(xm, 0, zm));
 			points[2].y = y;
 
-//			curve = new BeizerCurve(points, Roads.curveFunction());
-			curve = new StraightCurve(new Vector3f[] { points[0],points[3] }, Roads.CurveFunction());
+			curve = new BeizerCurve(points, Roads.CurveFunction());
+//			curve = new StraightCurve(new Vector3f[] { points[0],points[3] }, Roads.CurveFunction());
 			road.placePiece(new CurveQueueObj(0, curve, null), rootNode, phys, true);
 		}
 	}
@@ -193,35 +210,33 @@ public class HighwayWorld implements World {
 	private void fixTerrainHeights() {
 		//TODO fix: very slow
 		
-		// offset it by radius because in the loop we iterate through 2 radii
-        int size = 200;
-
-        float xStepAmount = terrain.getLocalScale().x;
-        float zStepAmount = terrain.getLocalScale().z;
+		Vector3f scale = terrain.getLocalScale();
+		
+        int size = 65;
         List<Vector2f> locs = new ArrayList<Vector2f>();
         List<Float> heights = new ArrayList<Float>();
         
         for (int z = -size; z < size; z++) {
             for (int x = -size; x < size; x++) {
 
-                float posX = x * xStepAmount;
-                float posZ = z * zStepAmount;
+                float posX = x * scale.x;
+                float posZ = z * scale.z;
 
                 // see if it is in the radius of the tool
             	Vector2f npos = new Vector2f(posX, posZ);
                 
                 float h = rayHeightAt(posX, posZ, "Quad");
                 if (h != 0) {
-                	heights.add(h - 0.2f);
+                	heights.add((h - 0.2f)/scale.y); //correctly scale the addition based on terrain scale
                 	locs.add(npos);
                 }
-                
             }
             H.e(z);
         }
 
         //update visuals
         terrain.setHeight(locs, heights);
+        terrain.recalculateAllNormals();
         terrain.updateModelBound();
 
         //update physics
@@ -231,15 +246,58 @@ public class HighwayWorld implements World {
 	    phys.add(c);
 	}
 	
+	@SuppressWarnings("unused")
+	private void saveTerrainToFile() {
+		float[] height_map = terrain.getHeightMap();
+		double p = Math.sqrt(height_map.length);
+		int q = (int) p;
+		
+		//Write to console
+		int counter = 0;
+		Float[][] m = new Float[q][q];
+		for (int x = 0; x < q; x++) {
+			for (int y = 0; y < q; y++) {
+				m[x][y] = height_map[counter];
+				counter++;
+			}
+		}
+		H.p(m, ",");
+				
+		//print the terrain to file (for debugging)
+		
+		BufferedImage image = new BufferedImage(q, q, BufferedImage.TYPE_INT_RGB);
+		counter = 0;
+		for (int x = 0; x < q; x++) {
+			for (int y = 0; y < q; y++) {
+				double value = Math.round(height_map[counter]);
+
+				int qq = Math.max((int) value, 0); // OR int qq = 255- (int) value;
+				try {
+					image.setRGB(x, y, new Color(qq, qq, qq).getRGB());
+				} catch (Exception e) {
+					H.e("ERROR " + e.getMessage());
+				}
+				counter = counter + 1;
+			}
+		}
+		File outputfile = new File("saved.png");
+		try {
+			ImageIO.write(image, "png", outputfile);
+		} catch (IOException ex) {
+			//this is debug code so, oh well
+			H.e("Image from terrain failed");
+		}
+	}
+	
 	//interface nodes
 	@Override
 	public Node getRootNode() { return rootNode; }
 	@Override
 	public Vector3f getWorldStart() {
-		if (terrain != null)
-			return new Vector3f(0,terrain.getHeight(new Vector2f(0, 3))+0.5f,3);
-		else 
-			return new Vector3f(0,0.5f,0);
+		if (terrain != null) {
+			return new Vector3f(0, rayHeightAt(0, 3, null)+0.5f, 3);
+		} else 
+			return new Vector3f(0, 0.5f, 0);
 	}
 	@Override
 	public Matrix3f getWorldRot() { return new Matrix3f(Matrix3f.IDENTITY); }
@@ -248,7 +306,14 @@ public class HighwayWorld implements World {
 	@Override
 	public void reset() { }
 	@Override
-	public void cleanup() { isInit = false; }
+	public void cleanup() { 
+		isInit = false;
+		
+		phys.remove(c);
+		terrain.removeControl(c);
+		rootNode.detachChild(terrain);
+		rootNode.detachAllChildren();
+	}
 	@Override
 	public Vector3f getNextPieceClosestTo(Vector3f pos) { return null; }
 
--- src/world/curve/PerlinNoise.java
+++ src/world/curve/PerlinNoise.java
@@ -1,26 +1,36 @@
 package world.curve;
 
-import com.jme3.math.FastMath;
+import com.jme3.math.Vector2f;
 import com.jme3.terrain.heightmap.AbstractHeightMap;
+
+import game.H;
 
 public class PerlinNoise extends AbstractHeightMap {
 
-	//http://stackoverflow.com/questions/4051027/algorithm-for-creating-infinite-terrain-landscape-surface
-	//http://gamedev.stackexchange.com/questions/93831/generate-next-chunk-with-perlin-noise
-	//http://flafla2.github.io/2014/08/09/perlinnoise.html
-	//https://gist.github.com/Flafla2/f0260a861be0ebdeef76
+	//http://stackoverflow.com/questions/4753055/perlin-noise-generation-for-terrain
+	//	(note this isn't real perlin noise but will do)
+	private double persistence, frequency, amplitude;
+	private int octaves, randomseed;
 	
-	//http://devmag.org.za/2009/04/25/perlin-noise/
-	public PerlinNoise(int size) {
+	private Vector2f origin;
+	
+	public PerlinNoise(int size, double persistence, double freq, double amp, int octave) {
 		this.size = size;
-		this.width = size;
-		this.height = size;
+		
+		this.persistence = persistence;//1;
+		this.frequency = freq;//0.1f;
+		this.amplitude = amp;//2;
+		this.octaves = octave;//2;
+		this.randomseed = 0;
+		
+		this.origin = Vector2f.ZERO;
 	}
-
-	//TODO (is generating distinct lines..)
 	
-	private int width;
-	private int height;
+	public void SetOrigin(Vector2f org) {
+		if (org == null)
+			throw new IllegalArgumentException();
+		this.origin = org;
+	}
 	
 	@Override
 	public boolean load() {
@@ -29,110 +39,112 @@ public class PerlinNoise extends AbstractHeightMap {
 		
 		heightData = new float[size*size];
 
-		float[][] baseNoise = GenerateWhiteNoise(width, height);
-		float[][] data = GeneratePerlinNoise(baseNoise, 4);
-		
 		for (int y = 0; y < size; y++) {
 			for (int x = 0; x < size; x++) {
-				heightData[x+y*size] = data[y][x];
+				Vector2f p = new Vector2f(x, y).add(origin); //why isnt there a Vector2f.add(float, float) ?
+				heightData[x+y*size] = (float)getHeightAt(p);
 			}
 		}
 		
 		return true;
 	}
 	
-	private static float[][] Empty2DArray(int width, int height) {
-		float[][] arrays = new float[width][1];
-		for (int i = 0; i < width; i++)
-			arrays[i] = new float[height];
-		return arrays;
+	private double getHeightAt(Vector2f pos)
+	{
+		return amplitude * total(pos.x, pos.y);
 	}
 	
-	private float[][] GeneratePerlinNoise(float[][] baseNoise, int octaveCount)
-    {
-        int width = baseNoise.length;
-        int height = baseNoise[0].length;
+	double total(double i, double j)
+	{
+		//properties of one octave (changing each loop)
+		double t = 0.0f;
+		double _amplitude = 1;
+		double freq = frequency;
 
-        float[][][] smoothNoise = new float[octaveCount][][]; //an array of 2D arrays containing
+		for(int k = 0; k < octaves; k++) 
+		{
+			t += getValue(j * freq + randomseed, i * freq + randomseed) * _amplitude;
+			_amplitude *= persistence;
+			freq *= 2;
+		}
 
-        float persistance = 0.7f;
-
-        //generate smooth noise
-        for (int i = 0; i < octaveCount; i++)
-            smoothNoise[i] = GenerateSmoothNoise(baseNoise, i);
-
-        float[][] perlinNoise = Empty2DArray(width, height); //an array of floats initialised to 0
-
-        float amplitude = 1.0f;
-        float totalAmplitude = 0.0f;
-        
-        //blend noise together
-        for (int octave = octaveCount - 1; octave >= 0; octave--)
-        {
-            amplitude *= persistance;
-            totalAmplitude += amplitude;
-
-            for (int i = 0; i < width; i++)
-                for (int j = 0; j < height; j++)
-                    perlinNoise[i][j] += smoothNoise[octave][i][j] * amplitude;
-        }
-
-        //normalisation
-        for (int i = 0; i < width; i++)
-            for (int j = 0; j < height; j++)
-                perlinNoise[i][j] /= totalAmplitude;
-
-        return perlinNoise;
-    }
+		return t;
+	}
 	
-	private float[][] GenerateSmoothNoise(float[][] baseNoise, int octave)
-    {
-        int width = baseNoise.length;
-        int height = baseNoise[0].length;
+	double getValue(double x, double y)
+	{
+		int Xint = (int)x;
+		int Yint = (int)y;
+		double Xfrac = x - Xint;
+		double Yfrac = y - Yint;
 
-        float[][] smoothNoise = Empty2DArray(width, height);
-
-        int samplePeriod = 1 << octave; // calculates 2 ^ k
-        float sampleFrequency = 1.0f / samplePeriod;
-
-        for (int i = 0; i < width; i++)
-        {
-            //calculate the horizontal sampling indices
-            int sample_i0 = (i / samplePeriod) * samplePeriod;
-            int sample_i1 = (sample_i0 + samplePeriod) % width; //wrap around
-            float horizontal_blend = (i - sample_i0) * sampleFrequency;
-
-            for (int j = 0; j < height; j++)
-            {
-                //calculate the vertical sampling indices
-                int sample_j0 = (j / samplePeriod) * samplePeriod;
-                int sample_j1 = (sample_j0 + samplePeriod) % height; //wrap around
-                float vertical_blend = (j - sample_j0) * sampleFrequency;
-
-                //blend the top two corners
-                float top = FastMath.interpolateLinear(baseNoise[sample_i0][sample_j0],
-                    baseNoise[sample_i1][sample_j0], horizontal_blend);
-
-                //blend the bottom two corners
-                float bottom = FastMath.interpolateLinear(baseNoise[sample_i0][sample_j1],
-                    baseNoise[sample_i1][sample_j1], horizontal_blend);
-
-                //final blend
-                smoothNoise[i][j] = FastMath.interpolateLinear(top, bottom, vertical_blend);
-            }
-        }
-        
-        return smoothNoise;
-    }
+		//noise values
+		double n01 = noise(Xint-1, Yint-1);
+		double n02 = noise(Xint+1, Yint-1);
+		double n03 = noise(Xint-1, Yint+1);
+		double n04 = noise(Xint+1, Yint+1);
+		double n05 = noise(Xint-1, Yint);
+		double n06 = noise(Xint+1, Yint);
+		double n07 = noise(Xint, Yint-1);
+		double n08 = noise(Xint, Yint+1);
+		double n09 = noise(Xint, Yint);
+		
+		double n12 = noise(Xint+2, Yint-1);
+		double n14 = noise(Xint+2, Yint+1);
+		double n16 = noise(Xint+2, Yint);
+		
+		double n23 = noise(Xint-1, Yint+2);
+		double n24 = noise(Xint+1, Yint+2);
+		double n28 = noise(Xint, Yint+2);
 	
-	private float[][] GenerateWhiteNoise(int width, int height)
-    {
-        float[][] noise = Empty2DArray(width, height);
+		double n34 = noise(Xint+2, Yint+2);
 
-        for (int i = 0; i < width; i++)
-            for (int j = 0; j < height; j++)
-                noise[i][j] = (float)FastMath.rand.nextFloat() % 1;
+		//find the noise values of the four corners
+		double x0y0 = 0.0625*(n01+n02+n03+n04) + 0.125*(n05+n06+n07+n08) + 0.25*(n09);  
+		double x1y0 = 0.0625*(n07+n12+n08+n14) + 0.125*(n09+n16+n02+n04) + 0.25*(n06);  
+		double x0y1 = 0.0625*(n05+n06+n23+n24) + 0.125*(n03+n04+n09+n28) + 0.25*(n08);  
+		double x1y1 = 0.0625*(n09+n16+n28+n34) + 0.125*(n08+n14+n06+n24) + 0.25*(n04);  
 
-        return noise;
-    }
+		//interpolate between those values according to the x and y fractions
+		double v1 = interpolate(x0y0, x1y0, Xfrac); //interpolate in x direction (y)
+		double v2 = interpolate(x0y1, x1y1, Xfrac); //interpolate in x direction (y+1)
+		double fin = interpolate(v1, v2, Yfrac);  //interpolate in y direction
+
+		return fin;
+	}
+	
+	private double interpolate(double x, double y, double a)
+	{
+		double negA = 1.0 - a;
+		double negASqr = negA * negA;
+		double fac1 = 3.0 * (negASqr) - 2.0 * (negASqr * negA);
+		double aSqr = a * a;
+		double fac2 = 3.0 * aSqr - 2.0 * (aSqr * a);
+
+		return x * fac1 + y * fac2; //add the weighted factors
+	}
+	
+	private double noise(int x, int y) {
+		int n = x + y * 57;
+		n = (n << 13) ^ n;
+		int t = (n * (n * n * 15731 + 789221) + 1376312589) & 0x7fffffff;
+		return 1d - ((double)t * 0.931322574615478515625e-9);
+	}
+	
+	public void Print() {
+		float[] height_map = getHeightMap();
+		double p = Math.sqrt(height_map.length);
+		int q = (int) p;
+		
+		//Write to console
+		int counter = 0;
+		Float[][] m = new Float[q][q];
+		for (int x = 0; x < q; x++) {
+			for (int y = 0; y < q; y++) {
+				m[x][y] = height_map[counter];
+				counter++;
+			}
+		}
+		H.p(m, ",");
+	}
 }
