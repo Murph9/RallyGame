b9c84e55bebee3631278b90c9a846f9fd960ec3c
Murph9 <murph9y@gmail.com>
2018-12-12 21:47:04 +1100

    Update polygon smoothing class, still not used though

--- src/world/track/BezierPolygonInterpolation.java
+++ src/world/track/BezierPolygonInterpolation.java
@@ -13,69 +13,63 @@ public class BezierPolygonInterpolation {
 
 	public static void BezierCurvePolygonInterpolation()
     {
-		BezierPolygonInterpolation.GetBezierCurves(
+		BezierPolygonInterpolation.GetBezierCurvesN(
 		Arrays.asList(        
-            new Vector3f(-2,0,0),
-            new Vector3f(5,0,0),
-            new Vector3f(2.5f,0,2)
+            new Vector3f(-1,0,1),
+            new Vector3f(-1,0,-1),
+            new Vector3f(1,0,-1),
+            new Vector3f(1,0,1)
         ), 0.5f);
     }
 	
-	//TODO this is not complete, missing:
-	//- polygon not triangle
-	//- the final step where it generates the tracksegment from all the sides
-	
-    public static List<TrackSegment> GetBezierCurves(List<Vector3f> points, float k)
+	public static List<TrackSegment> GetBezierCurvesN(List<Vector3f> points, float k)
     {
-        if (points.size() != 3)
-            return null; //TODO hack for starting the logic working
-
         //See: http://www.antigrain.com/research/bezier_interpolation/
 
-        Log.p(points);
+		Log.p(points);
 
+		int count = points.size();
+		
         //calculate mid points [step 1]
-        List<Vector3f> midPoints = new LinkedList<Vector3f>();
-        midPoints.add(lerp(points.get(0), points.get(1), 0.5f)); //A
-        midPoints.add(lerp(points.get(1), points.get(2), 0.5f)); //B
-        midPoints.add(lerp(points.get(0), points.get(2), 0.5f)); //C
-        //[0-1,1-2,0-2]
-
+		List<Vector3f> midPoints = new LinkedList<Vector3f>();
+        for (int i = 0; i < count; i++)
+        {
+            midPoints.add(lerp(points.get(i), points.get((i + 1) % points.size()), 0.5f));
+        }
         Log.p(midPoints);
 
         //calculate proportional points of the mid points [step2]
         List<Vector3f> midmidPoints = new LinkedList<Vector3f>();
-        float diff02 = points.get(2).distance(points.get(0));
-        float diff12 = points.get(2).distance(points.get(1));
-        float diff01 = points.get(1).distance(points.get(0));
-
-        float diff = diff02 / (diff02 + diff01);
-        midmidPoints.add(lerp(midPoints.get(0), midPoints.get(2), diff)); //D
-
-        diff = diff01 / (diff12 + diff01);
-        midmidPoints.add(lerp(midPoints.get(1), midPoints.get(0), diff)); //E
-
-        diff = diff12 / (diff02 + diff12);
-        midmidPoints.add(lerp(midPoints.get(2), midPoints.get(1), diff)); //F
-        //[0,1,2]
+        for (int i = 0; i < count; i++)
+        {
+            float _diff = points.get(i).distance(points.get(Math.floorMod(i - 1, count)))/
+                       (points.get(i).distance(points.get(Math.floorMod(i - 1, count))) + points.get(i).distance(points.get((i + 1) % count)));
+            midmidPoints.add(lerp(midPoints.get(i), midPoints.get(Math.floorMod(i - 1, count)), _diff));
+        }
 
         Log.p(midmidPoints);
 
         //calculate the pos of the points moved from the control points [step 3]
         List<TrackSegment> curves = new LinkedList<TrackSegment>();
 
-        Vector3f dir01 = midmidPoints.get(0).subtract(midPoints.get(0));
-        Vector3f dir10 = midmidPoints.get(1).subtract(midPoints.get(0));
-        curves.add(new TrackSegmentStraight(new Vector3f[] {
-            points.get(0), points.get(0).add(dir01), points.get(1).add(dir10), points.get(1)
-        }, (BiFunction<Vector3f, Vector3f, TrackSlice>)null));
+        for (int i = 0; i < count; i++)
+        {
+            Vector3f dirab = midmidPoints.get(i).subtract(midPoints.get(i)).mult(k);
+            Vector3f dirba = midmidPoints.get((i + 1) % count).subtract(midPoints.get(i)).mult(k);
+            curves.add(new TrackSegmentStraight(new Vector3f[]
+            {
+                points.get(i), points.get(i).add(dirab), points.get((i + 1) % count).add(dirba), points.get((i + 1) % count)
+            }, (BiFunction<Vector3f, Vector3f, TrackSlice>)null));
+        }
 
-        Log.p(curves.get(0).getControlPoints());
+        for (int i = 0; i < points.size(); i++)
+        {
+            Log.p(curves.get(i).getControlPoints(), "curves[" + i + "]");
+        }
 
         return curves;
     }
 
-    //TODO the real Vector3f class handles all this..
     private static Vector3f lerp(Vector3f a, Vector3f b, float t)
     {
     	try {
