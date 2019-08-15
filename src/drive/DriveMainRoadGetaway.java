dcfc5ae1f9bc82d9a86653ed22e6922dd8387ed9
murph9 <murph9y@gmail.com>
2019-08-15 19:17:25 +1000

    Add actual game to gateway state

--- src/drive/DriveMainRoadGetaway.java
+++ src/drive/DriveMainRoadGetaway.java
@@ -6,29 +6,63 @@ import java.util.function.BiFunction;
 
 import com.jme3.app.Application;
 import com.jme3.app.state.AppStateManager;
+import com.jme3.math.FastMath;
+import com.jme3.math.Matrix3f;
+import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
+import com.jme3.system.AppSettings;
+import com.simsilica.lemur.Container;
+import com.simsilica.lemur.Label;
 
 import car.ai.CarAI;
 import car.ai.DriveAlongAI;
 import car.data.Car;
 import car.ray.RayCarControl;
 import game.App;
+import helper.H;
+import helper.Log;
+import world.wp.DefaultBuilder;
 import world.wp.WP.DynamicType;
 
 public class DriveMainRoadGetaway extends DriveBase {
 
-	//single straight highway with traffic cars that just stay in their lane
-	//one car trying to slow you down
-	
-	private final Car chaser;
+	// goal:
+	// single straight highway with traffic cars that just stay in their lane (going both ways)
+	// one (heavier) car trying to slow you down
+	// more traffic/higher speed as the game goes on
+
+	//Other options for when to kill the player:
+	// - touching the chase car
+	// - stopping
+	// - hitting traffic
+
+	private static final int TRAFFIC_COUNT = 6;
+	private static final float TRAFFIC_BUFFER = 100;
+	private static final float HUNTER_BUFFER = 25;
+
+	private final Car hunterType;
 	private final int maxCount;
+
+	private RayCarControl hunter;
 	private int nextId;
+
+	private boolean readyToSpawnTraffic = false;
+	private boolean readyForHunter = false;
+
+	private float life;
+
+	// GUI objects
+	private Container display;
+	private Label scoreLabel;
+	private Label lifeLabel;
 	
 	public DriveMainRoadGetaway() {
-		super(Car.Rocket, DynamicType.MainRoad.getBuilder());
+		super(Car.Normal, DynamicType.MainRoad.getBuilder());
 		
-		maxCount = 8; //6 + me + chaser
-		chaser = Car.Rally;
+		maxCount = 2 + TRAFFIC_COUNT; // me chaser + x
+		hunterType = Car.Rally;
+
+		life = 10; // seconds of 'slowness' is your life
 	}
 	
 	@Override
@@ -36,32 +70,87 @@ public class DriveMainRoadGetaway extends DriveBase {
 		super.initialize(stateManager, app);
 		
 		//add the chase car
-		this.cb.addCar(1, chaser, world.getStartPos().add(0, 0, -10), world.getStartRot(), false, null);
+		hunter = this.cb.addCar(1, hunterType, new Vector3f(HUNTER_BUFFER - 10, 0.3f, 0), world.getStartRot(), false, null);
 		nextId = 2;
+
+		display = new Container();
+		display.addChild(new Label("Score: "));
+		scoreLabel = display.addChild(new Label("0"), 1);
+		display.addChild(new Label("Life: "));
+		lifeLabel = display.addChild(new Label("0"), 1);
+		
+		AppSettings set = app.getContext().getSettings();
+		display.setLocalTranslation(new Vector3f(set.getWidth() / 2, set.getHeight(), 0));
+		this.app.getGuiNode().attachChild(display);
 	}
 	
 	@Override
 	public void update(float tpf) {
 		super.update(tpf);
 		
-		if (this.cb.getCount() < maxCount && App.getFrameCount() % 60 == 0) {
-			final float z = nextXOff();
-			final boolean posDir = dirGivenCarNum(this.cb.getCount()); //TODO use when they go in reverse
-			BiFunction<RayCarControl, RayCarControl, CarAI> ai = (me, target) -> new DriveAlongAI(me, (vec) -> {
-				return new Vector3f(vec.x+20, 0, z);
-			}); //next pos math
+		RayCarControl player = this.cb.get(0);
+		RayCarControl chaser = this.cb.get(1);
+
+		float distanceFromStart = getPlayerDistanceFromStart(player);
+		float playerSpeed = player.getCurrentVehicleSpeedKmHour();
+
+		this.scoreLabel.setText(((int) distanceFromStart)+"");
+		this.lifeLabel.setText(((int) (life*100))+"");
+
+		//environment:
+		readyForHunter = distanceFromStart > HUNTER_BUFFER;
+		if (!readyForHunter) {
+			// the chase car must stay still, for now
+			hunter.setPhysicsLocation(new Vector3f(HUNTER_BUFFER-10, 0.3f, 0));
+			hunter.setPhysicsRotation(this.world.getStartRot());
+			Log.p("still");
+			return;
+		}
+
+		readyToSpawnTraffic = distanceFromStart > TRAFFIC_BUFFER;
+		if (!readyToSpawnTraffic) 
+			return;
+
+		//spawn more if required
+		if (this.cb.getCount() < maxCount && App.getFrameCount() % 10 == 0) {
+			final float z = nextZOff();
+			final boolean spawnPlayerDirection = spawnPlayerDirection();
+
+			BiFunction<RayCarControl, RayCarControl, CarAI> ai = (me, target) -> { 
+				DriveAlongAI daai = new DriveAlongAI(me, (vec) -> {
+					return new Vector3f(vec.x+20*(spawnPlayerDirection ? 1: -1), 0, z); // next pos math
+				});
+				daai.setMaxSpeed(27.777f); //100km/h
+				return daai;
+			};
 			
+			//closest piece from the start of the end of the world (as its always increasing in a straight line down positive x)
+			//and then subtract a bit so that it doesn't fall off
+			Vector3f spawnPos = ((DefaultBuilder) this.world).getNextPieceClosestTo(new Vector3f(100000, 0, 0))
+					.add(-20, 0, z); //offset
+			Matrix3f spawnDir = spawnPlayerDirection
+				? world.getStartRot()
+				: new Quaternion().fromAngleAxis(FastMath.PI, Vector3f.UNIT_Y).toRotationMatrix().mult(world.getStartRot());
+
 			//calculate spawn pos
-			RayCarControl c = this.cb.addCar(nextId, Car.Runner, new Vector3f(this.cb.get(0).getPhysicsLocation().x - 5, 0.3f, z), world.getStartRot(), false, ai);
+			RayCarControl c = this.cb.addCar(nextId, Car.WhiteSloth, spawnPos, spawnDir, false, ai);
 			c.setLinearVelocity(new Vector3f(20, 0, 0));
 			nextId++;
 		}
-
 		
 		//check if any traffic is falling and remove them
 		List<RayCarControl> toKill = new ArrayList<RayCarControl>();
 		for (RayCarControl c: this.cb.getAll()) {
-			if (c.up != null && (c.getPhysicsLocation().y < -10 || c.up.y < 0) && c != this.cb.get(0)) {
+			if (c == player) continue; 
+			
+			if (c.up != null && (c.getPhysicsLocation().y < -10 || c.up.y < 0)) {
+				if (c == chaser) {
+					//we only reset their position behind the player at the same speed, please don't delete them
+					c.setPhysicsLocation(player.getPhysicsLocation().add(-15, 0, 0));
+					c.setLinearVelocity(player.getLinearVelocity());
+					continue;
+				}
+			
 				toKill.add(c);
 			}
 		}
@@ -69,10 +158,26 @@ public class DriveMainRoadGetaway extends DriveBase {
 			//killed (out of loop)
 			cb.removeCar(c);
 		}
+
+
+		// scoring:
+		if (readyToSpawnTraffic && playerSpeed < 10) {
+			// loaded and ready to lose score
+			life -= tpf;
+		}
+
+		if (life < 0) {
+			//TODO killing the game seems good enough.
+			this.cb.cleanup();
+		}
 	}
 	
+	private boolean spawnPlayerDirection() {
+		return FastMath.rand.nextBoolean();
+	}
+
 	private int CarSpawnCounter = 0; 
-	private float nextXOff() {
+	private float nextZOff() {
 		CarSpawnCounter++;
 		int i = CarSpawnCounter % 6;
 		if (i == 5)
@@ -90,7 +195,8 @@ public class DriveMainRoadGetaway extends DriveBase {
 		else 
 			return 8f;
 	}
-	private boolean dirGivenCarNum(int i) {
-		return i > 0;
+
+	private float getPlayerDistanceFromStart(RayCarControl car) {
+		return FastMath.abs(this.world.getStartPos().x - car.getPhysicsLocation().x);
 	}
 }
