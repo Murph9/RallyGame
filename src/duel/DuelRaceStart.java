173fa17fd138cd0ba787f4476c55559b7a59cee1
murph9 <murph9y@gmail.com>
2019-11-28 20:20:40 +1100

    Add car preview to DuelStart

--- src/duel/DuelRaceStart.java
+++ src/duel/DuelRaceStart.java
@@ -7,12 +7,21 @@ import com.simsilica.lemur.Button;
 import com.simsilica.lemur.Container;
 import com.simsilica.lemur.Label;
 
+import car.CarBuilder;
+import game.BasicCamera;
 import helper.H;
+import world.StaticWorld;
+import world.StaticWorldBuilder;
+import world.World;
 
 public class DuelRaceStart extends BaseAppState {
 
     private final IDuelFlow flow;
+    
+    private World world;
+    private CarBuilder cb;
     private Container window;
+    private BasicCamera camera;
 
     public DuelRaceStart(IDuelFlow flow) {
         this.flow = flow;
@@ -21,6 +30,8 @@ public class DuelRaceStart extends BaseAppState {
     @SuppressWarnings("unchecked") // button checked vargs
     @Override
     protected void initialize(Application app) {
+        cb = getState(CarBuilder.class);
+
         window = new Container();
         ((DuelApp) app).getGuiNode().attachChild(window);
 
@@ -35,11 +46,31 @@ public class DuelRaceStart extends BaseAppState {
 
         Vector3f middle = H.screenCenterMe(app.getContext().getSettings(), window.getPreferredSize());
         window.setLocalTranslation(middle);
+
+        //show the cars
+        world = new StaticWorldBuilder(StaticWorld.duct); // a boring world
+        getStateManager().attach(world);
+        Vector3f worldStart = world.getStartPos();
+
+        camera = new BasicCamera("Camera", app.getCamera(), worldStart.add(0, 5, 12), worldStart);
+        getStateManager().attach(camera);
+
+        cb.addCar(data.yourCar, worldStart.add(-3, 0, 0), world.getStartRot(), false);
+        cb.addCar(data.theirCar, worldStart.add(3, 0, 0), world.getStartRot(), false);
     }
 
     @Override
     protected void cleanup(Application app) {
         ((DuelApp) app).getGuiNode().detachChild(window);
+
+        getStateManager().detach(camera);
+        camera = null;
+
+        getStateManager().detach(world);
+        world = null;
+
+        cb.removeAll();
+        cb = null;
     }
 
     @Override
@@ -48,4 +79,5 @@ public class DuelRaceStart extends BaseAppState {
     @Override
     protected void onDisable() {
     }
+
 }
