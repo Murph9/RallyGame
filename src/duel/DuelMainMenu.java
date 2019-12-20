597dd167f1342b212b3c1513bdba0b8e13a26f39
murph9 <murph9y@gmail.com>
2019-12-20 17:57:16 +1100

    and clean duel main menu quit button

--- src/duel/DuelMainMenu.java
+++ src/duel/DuelMainMenu.java
@@ -10,7 +10,6 @@ import com.jme3.input.event.KeyInputEvent;
 import com.jme3.input.event.MouseButtonEvent;
 import com.jme3.input.event.MouseMotionEvent;
 import com.jme3.input.event.TouchEvent;
-import com.jme3.math.Vector3f;
 import com.simsilica.lemur.Button;
 import com.simsilica.lemur.Container;
 import com.simsilica.lemur.HAlignment;
@@ -20,13 +19,14 @@ import com.simsilica.lemur.style.ElementId;
 import car.CarBuilder;
 import car.CarStatsUI;
 import car.data.CarDataConst;
-import helper.H;
+import helper.Screen;
 
 public class DuelMainMenu extends BaseAppState implements RawInputListener {
 
     private final IDuelFlow flow;
     private final DuelData duelData;
-    private Container window;
+    private Container mainWindow;
+    private Container quitWindow;
 
     public DuelMainMenu(IDuelFlow flow, DuelData gameOverData) {
         this.flow = flow;
@@ -36,36 +36,40 @@ public class DuelMainMenu extends BaseAppState implements RawInputListener {
     @SuppressWarnings("unchecked") // button checked vargs
     @Override
     protected void initialize(Application app) {
-        window = new Container();
-        window.setBackground(DuelUiStyle.getBorderedNoBackground());
+        mainWindow = new Container();
+        mainWindow.setBackground(DuelUiStyle.getBorderedNoBackground());
 
-        Label l = window.addChild(new Label("Duel", new ElementId("title")));
-        l = window.addChild(new Label("Press any key to start"));
+        Label l = mainWindow.addChild(new Label("Duel", new ElementId("title")));
+        l = mainWindow.addChild(new Label("Press any key to start"));
         l.setTextHAlignment(HAlignment.Center);
 
-        Button b = window.addChild(new Button("Quit"));
+        if (duelData != null) {
+            mainWindow.addChild(new Label("Wins: " + duelData.wins));
+            CarBuilder cb = getState(CarBuilder.class);
+            CarDataConst data = cb.loadData(duelData.yourCar, duelData.yourAdjuster);
+            mainWindow.addChild(new CarStatsUI(app.getAssetManager(), data), 2, 0);
+        }
+
+        ((SimpleApplication) app).getGuiNode().attachChild(mainWindow);
+        app.getInputManager().addRawInputListener(this);
+
+        Screen screen = new Screen(app.getContext().getSettings());
+        screen.centerMe(mainWindow);
+
+        quitWindow = new Container();
+        Button b = quitWindow.addChild(new Button("Quit"));
         b.addClickCommands((source) -> {
             DuelResultData d = new DuelResultData();
             d.quitGame = true;
             flow.nextState(this, d);
         });
-
-        if (duelData != null) {
-            CarBuilder cb = getState(CarBuilder.class);
-            CarDataConst data = cb.loadData(duelData.yourCar, duelData.yourAdjuster);
-            window.addChild(new CarStatsUI(app.getAssetManager(), data), 2, 0);
-        }
-
-        ((SimpleApplication) app).getGuiNode().attachChild(window);
-        app.getInputManager().addRawInputListener(this);
-
-        Vector3f middle = H.screenCenterMe(app.getContext().getSettings(), window.getPreferredSize());
-        window.setLocalTranslation(middle);
+        ((SimpleApplication) app).getGuiNode().attachChild(quitWindow);
+        screen.topCenterMe(quitWindow);
     }
 
     @Override
     protected void cleanup(Application app) {
-        ((SimpleApplication) app).getGuiNode().detachChild(window);
+        ((SimpleApplication) app).getGuiNode().detachChild(mainWindow);
         app.getInputManager().removeRawInputListener(this);
     }
 
