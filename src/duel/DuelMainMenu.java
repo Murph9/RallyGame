4a9c81f893e76fb63fe0ba24c9b5109a1c99063b
murph9 <murph9y@gmail.com>
2020-01-03 18:09:51 +1100

    Add preview to start of Duel app like base app (and remove from base app)

--- src/duel/DuelMainMenu.java
+++ src/duel/DuelMainMenu.java
@@ -2,7 +2,9 @@ package duel;
 
 import com.jme3.app.Application;
 import com.jme3.app.SimpleApplication;
+import com.jme3.app.state.AppState;
 import com.jme3.app.state.BaseAppState;
+import com.jme3.bullet.BulletAppState;
 import com.jme3.input.RawInputListener;
 import com.jme3.input.event.JoyAxisEvent;
 import com.jme3.input.event.JoyButtonEvent;
@@ -19,14 +21,27 @@ import com.simsilica.lemur.style.ElementId;
 
 import car.CarBuilder;
 import car.CarStatsUI;
+import car.RotatesCarCamera;
+import car.ai.DriveAlongAI;
+import car.data.Car;
 import car.data.CarDataConst;
+import car.ray.RayCarControl;
 import helper.Screen;
+import service.LoadingScreen;
+import world.wp.DefaultBuilder;
+import world.wp.WP.DynamicType;
 
 public class DuelMainMenu extends BaseAppState implements RawInputListener {
 
     private final IDuelFlow flow;
     private final DuelData duelData;
     private final String version;
+
+    private final Car carType;
+    private final DefaultBuilder world;
+
+    private AppState camera;
+    private CarBuilder cb;
 
     private Container mainWindow;
     private Container altWindow;
@@ -35,16 +50,27 @@ public class DuelMainMenu extends BaseAppState implements RawInputListener {
         this.flow = flow;
         this.duelData = gameOverData;
         this.version = version;
+
+        carType = Car.Runner;
+        world = DynamicType.Valley.getBuilder();
+    }
+
+    @Override
+    protected void initialize(Application app) {
+        initMenu((SimpleApplication) app);
+        initBackground((SimpleApplication) app);
     }
 
     @SuppressWarnings("unchecked") // button checked vargs
-    @Override
-    protected void initialize(Application app) {
+    private void initMenu(SimpleApplication app) {
+        
+        getState(LoadingScreen.class).setEnabled(true);
+
         mainWindow = new Container();
         mainWindow.setBackground(DuelUiStyle.getBorderedNoBackground());
 
         mainWindow.addChild(new Label("Duel", new ElementId("title")));
-        
+
         if (duelData != null) {
             Button b = mainWindow.addChild(new Button("Retry"));
             b.addClickCommands((source) -> {
@@ -62,18 +88,19 @@ public class DuelMainMenu extends BaseAppState implements RawInputListener {
             app.getInputManager().addRawInputListener(this);
         }
 
-        ((SimpleApplication) app).getGuiNode().attachChild(mainWindow);
-        
+        app.getGuiNode().attachChild(mainWindow);
+
         altWindow = new Container();
         Button b = altWindow.addChild(new Button("Controls"), 0, 0);
-        Label l = new Label("move: wasd and arrows\nflip: f\nhandbrake: space\nreverse: leftshift (hold)\npause: esc\nnitro: either control\ntelemetry: home");
+        Label l = new Label("move: wasd and arrows\nflip: f\nhandbrake: space\n"
+                + "reverse: leftshift (hold)\npause: esc\nnitro: either control\ntelemetry: home");
         b.addClickCommands((source) -> {
             if (l.getParent() == null)
                 altWindow.addChild(l, 1, 0);
-            else 
+            else
                 altWindow.removeChild(l);
         });
-        
+
         b = altWindow.addChild(new Button("Quit"), 0, 1);
         b.addClickCommands((source) -> {
             DuelResultData d = new DuelResultData();
@@ -82,7 +109,25 @@ public class DuelMainMenu extends BaseAppState implements RawInputListener {
         });
 
         altWindow.addChild(new Label(this.version), 0, 2);
-        ((SimpleApplication) app).getGuiNode().attachChild(altWindow);
+        app.getGuiNode().attachChild(altWindow);
+    }
+
+    private void initBackground(SimpleApplication app) {
+        getStateManager().attach(world);
+
+        // build player
+        this.cb = getState(CarBuilder.class);
+        RayCarControl car = cb.addCar(this.carType, world.getStartPos(), world.getStartRot(), true);
+
+        // attach basic ai, for the view
+        DriveAlongAI ai = new DriveAlongAI(car, (vec) -> world.getNextPieceClosestTo(vec));
+        ai.setMaxSpeed(27.7778f); // 27.7 = 100 km/h
+        car.attachAI(ai, true);
+
+        this.camera = new RotatesCarCamera(app.getCamera(), car);
+        getStateManager().attach(this.camera);
+
+        getState(BulletAppState.class).setEnabled(true);
     }
 
     @Override
@@ -91,6 +136,15 @@ public class DuelMainMenu extends BaseAppState implements RawInputListener {
         n.detachChild(mainWindow);
         n.detachChild(altWindow);
         app.getInputManager().removeRawInputListener(this);
+
+        getStateManager().detach(world);
+        getState(BulletAppState.class).setEnabled(false);
+
+        getStateManager().detach(camera);
+        camera = null;
+
+        cb.removeAll();
+        cb = null;
     }
 
     @Override
@@ -133,12 +187,13 @@ public class DuelMainMenu extends BaseAppState implements RawInputListener {
     }
     @Override
     public void onTouchEvent(TouchEvent evt) {}
-    
+    //#endregion
+
     private void start() {
         if (isEnabled()) {
             DuelResultData d = new DuelResultData();
             flow.nextState(this, d);
+            this.setEnabled(false); //to prevent this being called twice
         }
     }
-    //#endregion
 }
--- src/game/Start.java
+++ src/game/Start.java
@@ -5,65 +5,28 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import com.jme3.app.Application;
-import com.jme3.app.state.AbstractAppState;
 import com.jme3.app.state.BaseAppState;
-import com.jme3.bullet.BulletAppState;
-import com.jme3.math.Quaternion;
-import com.jme3.math.Vector3f;
-import com.jme3.renderer.Camera;
-import com.jme3.renderer.RenderManager;
 import com.simsilica.lemur.Button;
 import com.simsilica.lemur.Container;
 import com.simsilica.lemur.Label;
 
-import car.CarBuilder;
-import car.ai.DriveAlongAI;
-import car.data.Car;
-import car.ray.RayCarControl;
 import drive.IFlow;
-import world.wp.DefaultBuilder;
-import world.wp.WP.DynamicType;
 
 public class Start extends BaseAppState {
 
 	private final IFlow flow;
 
-	private DefaultBuilder world;
-
-	private CarBuilder cb;
-	private Car carType;
-	
-	private StartCamera camera;
 	private Container myWindow;
 	
 	public Start(IFlow flow) {
 		super();
 		this.flow = flow;
-
-		carType = Car.Runner;
-		world = DynamicType.Valley.getBuilder();
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public void initialize(Application app) {
 		App myapp = ((App) app);
-
-		getStateManager().attach(world);
-		
-		// build player
-		this.cb = getState(CarBuilder.class);
-		RayCarControl car = cb.addCar(this.carType, world.getStartPos(), world.getStartRot(), true);
-
-		//attach basic ai, for the view
-		DriveAlongAI ai = new DriveAlongAI(car, (vec) -> world.getNextPieceClosestTo(vec));
-		ai.setMaxSpeed(27.7778f); // 27.7 = 100 km/h
-		car.attachAI(ai, true);
-		
-		this.camera = new StartCamera("start camera", app.getCamera(), car);
-		getStateManager().attach(this.camera);
-		
-		getState(BulletAppState.class).setEnabled(true);
 
 		//UI
 		myWindow = new Container();
@@ -95,29 +58,14 @@ public class Start extends BaseAppState {
 	}
 	
 	@Override
-	protected void onEnable() {
-		this.cb.setEnabled(true);
-	}
+	protected void onEnable() {}
 	@Override
-	protected void onDisable() {
-		this.cb.setEnabled(false);
-	}
-
+    protected void onDisable() {}
+    
 	@Override
 	public void cleanup(Application app) {
-		getState(BulletAppState.class).setEnabled(false);
-
-		getStateManager().detach(camera);
-		camera = null;
-		
-		myWindow.removeFromParent();
+        myWindow.removeFromParent();
 		myWindow = null;
-		
-		cb.removeAll();
-		cb = null;
-		
-		getStateManager().detach(world);
-		world = null;
 	}
 
 	private Map<String, Runnable> generateButtonMappings(IFlow flow) {
@@ -126,48 +74,5 @@ public class Start extends BaseAppState {
 			buttonActions.put(t.name(), () -> { flow.startCallback(t); });
 		}
 		return buttonActions;
-	}
-
-	class StartCamera extends AbstractAppState {
-		// render() based camera to prevent jumpiness
-
-		private final Camera c;
-		private final RayCarControl rcc;
-
-		private final Vector3f offset;
-		private final Vector3f lookAtHeight;
-
-		private float angle;
-
-		public StartCamera(String name, Camera c, RayCarControl rcc) {
-			super();
-
-			this.c = c;
-			this.rcc = rcc;
-
-			this.offset = new Vector3f(0, rcc.getCarData().cam_offsetHeight, rcc.getCarData().cam_offsetLength);
-			this.lookAtHeight = new Vector3f(0, rcc.getCarData().cam_lookAtHeight, 0);
-
-			this.c.setLocation(rcc.getRootNode().getLocalTranslation().add(offset));
-			this.c.lookAt(rcc.getRootNode().getLocalTranslation().add(lookAtHeight), Vector3f.UNIT_Y);
-		}
-
-		@Override
-		public void update(float tpf) {
-			if (!isEnabled())
-				return;
-			super.update(tpf);
-
-			angle += tpf / 8;
-		}
-
-		@Override
-		public void render(RenderManager rm) {
-			Vector3f movedOffset = new Quaternion().fromAngleAxis(angle, Vector3f.UNIT_Y).mult(this.offset);
-			Vector3f camPos = rcc.getRootNode().getLocalTranslation().add(movedOffset);
-
-			this.c.setLocation(camPos);
-			this.c.lookAt(rcc.getRootNode().getLocalTranslation().add(lookAtHeight), Vector3f.UNIT_Y);
-		}
 	}
 }
