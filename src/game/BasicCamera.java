c1f43bcad2a155ee1b2115e998c4ad343806ba31
murph9 <murph9y@gmail.com>
2020-01-03 18:11:00 +1100

    BasicCamera; update it have better render logic

--- src/game/BasicCamera.java
+++ src/game/BasicCamera.java
@@ -4,42 +4,45 @@ import com.jme3.app.Application;
 import com.jme3.app.state.BaseAppState;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.Camera;
+import com.jme3.renderer.RenderManager;
 
 public class BasicCamera extends BaseAppState {
 
-	private Camera c;
+    private final Camera c;
+
+    private Vector3f nextPos;
+    private Vector3f nextLookAt;
 	
 	public BasicCamera(String name, Camera c, Vector3f pos, Vector3f lookat) {
-		super();
-		
-		this.c = c;
-		this.c.setLocation(pos); //starting position of the camera
-		this.c.lookAt(lookat, Vector3f.UNIT_Y);
+        this.c = c;
+        this.nextPos = pos;
+        this.nextLookAt = lookat;
 	}
 
 	public void updatePosition(Vector3f pos, Vector3f lookAt) {
-		this.c.setLocation(pos);
-		this.c.lookAt(lookAt, Vector3f.UNIT_Y);
-
-		//Please avoid changing the camera too often 
-		//change to use the same logic as CarCamera.render
-		//otherwise the camera kind of jumps around on unstable framerates
+        nextPos = pos;
+		nextLookAt = lookAt;
 	}
 
+    @Override
+	public void render(RenderManager rm) {
+        if (this.nextPos != null) {
+            this.c.setLocation(this.nextPos);
+            this.nextPos = null;
+        }
+
+        if (this.nextLookAt != null) {
+            this.c.lookAt(this.nextLookAt, Vector3f.UNIT_Y);
+            this.nextLookAt = null;
+        }
+    }
+
 	@Override
-	protected void initialize(Application app) {
-		
-	}
+	protected void initialize(Application app) {}
 	@Override
-	protected void cleanup(Application app) {
-		
-	}
+	protected void cleanup(Application app) {}
 	@Override
-	protected void onEnable() {
-		
-	}
+	protected void onEnable() {}
 	@Override
-	protected void onDisable() {
-		
-	}
+	protected void onDisable() {}
 }
