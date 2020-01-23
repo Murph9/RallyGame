624f47aada9b42b602f22f3c0f64a3d81840c29c
murph9 <murph9y@gmail.com>
2020-01-23 18:36:30 +1100

    Clean MovingWorld

--- src/world/MovingWorld.java
+++ src/world/MovingWorld.java
@@ -4,14 +4,21 @@ import com.jme3.app.Application;
 import com.jme3.bullet.BulletAppState;
 import com.jme3.bullet.control.RigidBodyControl;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
 
+import car.CarBuilder;
 import effects.LoadModelWrapper;
 
 public class MovingWorld extends World {
+    
+    private static final float BOX_HEIGHT = 1;
+    private static final float BOX_SIZE = 20;
+    private static final float PLATFORM_SPEED = 5;
+    private static final float PLATFORM_RESET_TIME = 30;
     
     private Spatial startGeometry;
 	private Spatial box;
@@ -31,9 +38,11 @@ public class MovingWorld extends World {
 	public void initialize(Application app) {
 		super.initialize(app);
 
+        getState(CarBuilder.class).get(0).setPhysicsLocation(new Vector3f(0, BOX_HEIGHT + 0.5f, 0));
+
 		//still box
 		startGeometry = LoadModelWrapper.create(app.getAssetManager(), 
-			new Geometry("box", new Box(20, 0.25f, 20)), 
+			new Geometry("box", new Box(BOX_SIZE, BOX_HEIGHT, BOX_SIZE)), 
 			ColorRGBA.Green);
 		startGeometry.setLocalTranslation(0, 0, 0);
 		startGeometry.addControl(new RigidBodyControl(0));
@@ -43,7 +52,7 @@ public class MovingWorld extends World {
 
 		//moving box
 		box = LoadModelWrapper.create(app.getAssetManager(),
-			new Geometry("Box", new Box(20, 0.25f, 20)),
+			new Geometry("Box", new Box(BOX_SIZE, BOX_HEIGHT, BOX_SIZE)),
 			ColorRGBA.Cyan);
 		
 		RigidBodyControl rbc = new RigidBodyControl(10000);
@@ -61,19 +70,18 @@ public class MovingWorld extends World {
 		
 	}
 
-
-	private float PLATFORM_SPEED = 5;
-	private float PLATFORM_RESET_TIME = 30;
 	@Override
 	public void update(float tpf) {
-		//always moving but not rotating (kinematic please)
-		
-		// box.getControl(RigidBodyControl.class).setAngularVelocity(new Vector3f(0, 0, 0));
-		box.getControl(RigidBodyControl.class).setLinearVelocity(new Vector3f(0, 0, PLATFORM_SPEED));
+        RigidBodyControl control = box.getControl(RigidBodyControl.class);
+
+		//always moving but not rotating much
+		control.setAngularVelocity(new Vector3f(0, 0, 0));
+		control.setLinearVelocity(new Vector3f(0, 0, PLATFORM_SPEED));
 		resetTimer += tpf;
 		if (resetTimer > PLATFORM_RESET_TIME) {
 			resetTimer = 0;
-			box.getControl(RigidBodyControl.class).setPhysicsLocation(new Vector3f(40.1f, 0, -PLATFORM_RESET_TIME/2*PLATFORM_SPEED));
+            control.setPhysicsLocation(new Vector3f(BOX_SIZE * 2 + 0.1f, 0, -PLATFORM_RESET_TIME / 2 * PLATFORM_SPEED));
+            control.setPhysicsRotation(new Quaternion());
 		}
 	}
 	
