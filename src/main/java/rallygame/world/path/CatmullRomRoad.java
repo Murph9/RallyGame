3b58ea20ced3430e20ffcc4624f4e51bdcbc6d2e
murph9 <murph9y@gmail.com>
2020-04-15 10:50:04 +1000

    Refactor CatrmullRomRoad to be cleaner and correctly use the control point ends

--- src/main/java/rallygame/world/path/CatmullRomRoad.java
+++ src/main/java/rallygame/world/path/CatmullRomRoad.java
@@ -1,6 +1,5 @@
 package rallygame.world.path;
 
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -11,6 +10,7 @@ import com.jme3.math.Vector3f;
 import com.jme3.math.Spline.SplineType;
 import com.jme3.scene.Mesh;
 import com.jme3.scene.VertexBuffer;
+import com.jme3.util.BufferUtils;
 
 public class CatmullRomRoad extends Mesh {
 
@@ -18,10 +18,8 @@ public class CatmullRomRoad extends Mesh {
     // https://github.com/jMonkeyEngine/jmonkeyengine/blob/master/jme3-core/src/main/java/com/jme3/scene/shape/Curve.java
 
     private final Spline spline;
-    private final Vector3f temp1 = new Vector3f();
-    private final Vector3f temp2 = new Vector3f();
     private final float width;
-    private final float[] vertexArray;
+    private final Vector3f[] vertexArray;
 
     /** Road based on a CatmullRom spline, with width. Remember it can't use the first and last ones. */
     public CatmullRomRoad(Spline spline, int nbSubSegments, float width) {
@@ -29,53 +27,49 @@ public class CatmullRomRoad extends Mesh {
         this.width = width;
         if (spline.getType() != SplineType.CatmullRom)
             throw new IllegalArgumentException("Only " + SplineType.CatmullRom + " please");
-        if (spline.getControlPoints().size() < 4)
-            throw new IllegalArgumentException("Spline must have at least 2 sections");
+        if (spline.getControlPoints().size() < 2)
+            throw new IllegalArgumentException("Spline must have at least 2 control points making 1 section [start->end]");
         
-        this.vertexArray = new float[((spline.getControlPoints().size() - 2) * nbSubSegments) * 3 * 2 * 3];
+        final int cpCount = spline.getControlPoints().size() - 1;
+        this.vertexArray = new Vector3f[cpCount * nbSubSegments * 3 * 2];
 
+        final Vector3f temp1 = new Vector3f();
+        final Vector3f temp2 = new Vector3f();
         Vector3f temp3 = null;
         Vector3f temp4 = null;
         int i = 0;
-        int cptCP = 0;
-        for (Iterator<Vector3f> it = spline.getControlPoints().iterator(); it.hasNext(); cptCP++) {
-            it.next();
-            if (it.hasNext()) {
-                for (int j = 0; j < nbSubSegments; j++) {
-                    setNormalPointsInto(temp1, temp2, (float) j / nbSubSegments, cptCP);
-                    if (temp3 != null && temp4 != null) {
-                        vertexArray[i++] = temp3.getX();
-                        vertexArray[i++] = temp3.getY();
-                        vertexArray[i++] = temp3.getZ();
-                        
-                        vertexArray[i++] = temp4.getX();
-                        vertexArray[i++] = temp4.getY();
-                        vertexArray[i++] = temp4.getZ();
-    
-                        vertexArray[i++] = temp1.getX();
-                        vertexArray[i++] = temp1.getY();
-                        vertexArray[i++] = temp1.getZ();
-    
+        for (int cptCP = 0; cptCP < cpCount; cptCP++) {
+            for (int j = 0; j < nbSubSegments; j++) {
+                setNormalPointsInto(temp1, temp2, (float) j / nbSubSegments, cptCP);
+                if (temp3 != null && temp4 != null) {
+                    vertexArray[i++] = temp3.clone();
+                    vertexArray[i++] = temp4.clone();
+                    vertexArray[i++] = temp1.clone();
 
-                        vertexArray[i++] = temp4.getX();
-                        vertexArray[i++] = temp4.getY();
-                        vertexArray[i++] = temp4.getZ();
-
-                        vertexArray[i++] = temp1.getX();
-                        vertexArray[i++] = temp1.getY();
-                        vertexArray[i++] = temp1.getZ();
-
-                        vertexArray[i++] = temp2.getX();
-                        vertexArray[i++] = temp2.getY();
-                        vertexArray[i++] = temp2.getZ();
-                    }
-                    
-                    temp3 = temp1.clone();
-                    temp4 = temp2.clone();
+                    vertexArray[i++] = temp4.clone();
+                    vertexArray[i++] = temp1.clone();
+                    vertexArray[i++] = temp2.clone();
                 }
+                
+                temp3 = temp1.clone();
+                temp4 = temp2.clone();
             }
         }
-        this.setBuffer(VertexBuffer.Type.Position, 3, vertexArray);
+
+        //then set the last point, just back from the end of the last segment
+        //this completes the last subsegment, the last segment
+        setNormalPointsInto(temp1, temp2, 1, cpCount-1);
+        if (temp3 != null && temp4 != null) {
+            vertexArray[i++] = temp3.clone();
+            vertexArray[i++] = temp4.clone();
+            vertexArray[i++] = temp1.clone();
+
+            vertexArray[i++] = temp4.clone();
+            vertexArray[i++] = temp1.clone();
+            vertexArray[i++] = temp2.clone();
+        }
+        
+        this.setBuffer(VertexBuffer.Type.Position, 3, BufferUtils.createFloatBuffer(vertexArray));
         this.setMode(Mesh.Mode.Triangles);
         this.updateBound();
         this.updateCounts();
@@ -94,16 +88,13 @@ public class CatmullRomRoad extends Mesh {
     }
 
     public List<Vector3f[]> getMeshAsQuads() {
-        //[0, 1, 2, 3, 4, 5, 6, 7, 8, 9] -> [[0,1,2,3], [1,2,3,4], [2,3,4,5], ...]
-
         List<Vector3f[]> results = new LinkedList<>();
-        for (int i = 0; i < vertexArray.length-4*3; i+=3*4) {
-            results.add(new Vector3f[]{
-                    new Vector3f(vertexArray[i], vertexArray[i + 1], vertexArray[i + 2]),
-                    new Vector3f(vertexArray[i + 3], vertexArray[i + 4], vertexArray[i + 5]),
-                    new Vector3f(vertexArray[i + 6], vertexArray[i + 7], vertexArray[i + 8]),
-                    new Vector3f(vertexArray[i + 9], vertexArray[i + 10], vertexArray[i + 11])
-            });
+        for (int i = 0; i < vertexArray.length-1; i+=6) {
+            Vector3f[] list = new Vector3f[] {
+                vertexArray[i], vertexArray[i+1], vertexArray[i+2], vertexArray[i+5]
+            };
+
+            results.add(list);
         }
         
         return results;
