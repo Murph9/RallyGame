33ad77e7e97d1990866133b60669e0a4bb529abb
murph9 <murph9y@gmail.com>
2020-04-15 10:50:29 +1000

    Add CatmullRomRoad test, simple so i could test it

---
+++ src/test/java/rallygame/CatmullRomRoadTest.java
@@ -0,0 +1,45 @@
+package rallygame;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import com.jme3.math.Spline;
+import com.jme3.math.Vector3f;
+import com.jme3.math.Spline.SplineType;
+
+import org.junit.jupiter.api.Test;
+
+import rallygame.world.path.CatmullRomRoad;
+
+public class CatmullRomRoadTest {
+
+    @Test
+    public void getQuads() {
+        Vector3f[] list = new Vector3f[] { new Vector3f(0, 0, 0), new Vector3f(0, 0, 1) };
+        Spline s3 = new Spline(SplineType.CatmullRom, list, 1, false); // [0-1], 1 is more smooth
+
+        CatmullRomRoad road = new CatmullRomRoad(s3, 1, 1);
+
+        List<Vector3f[]> quads = road.getMeshAsQuads();
+
+        assertEquals(1, quads.size());
+
+        Vector3f[] firstQuad = quads.get(0);
+        assertEquals(0.5f, firstQuad[0].x, 0.0001f);
+        assertEquals(0f, firstQuad[0].y, 0.0001f);
+        assertEquals(0f, firstQuad[0].z, 0.0001f);
+
+        assertEquals(-0.5f, firstQuad[1].x, 0.0001f);
+        assertEquals(0f, firstQuad[1].y, 0.0001f);
+        assertEquals(0f, firstQuad[1].z, 0.0001f);
+
+        assertEquals(0.5f, firstQuad[2].x, 0.0001f);
+        assertEquals(0f, firstQuad[2].y, 0.0001f);
+        assertEquals(1f, firstQuad[2].z, 0.0001f);
+
+        assertEquals(-0.5f, firstQuad[3].x, 0.0001f);
+        assertEquals(0f, firstQuad[3].y, 0.0001f);
+        assertEquals(1f, firstQuad[3].z, 0.0001f);
+    }
+}
