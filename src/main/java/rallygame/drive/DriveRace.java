97c3fcdf9f73301eb7fa54281c17bb7c1151fe2f
Jake Murphy <jake.murphy@servcorp.com.au>
2020-10-12 13:38:37 +1100

    Increate drive race checkpoint size

--- src/main/java/rallygame/drive/DriveRace.java
+++ src/main/java/rallygame/drive/DriveRace.java
@@ -88,7 +88,7 @@ public class DriveRace extends DriveBase implements PauseState.ICallback, ICheck
         }
         
         progress = new CheckpointProgress(CheckpointProgress.Type.Lap, checkpoints, cm.getAll(), cm.getPlayer());
-        progress.setCheckpointModel(CheckpointProgress.GetDefaultCheckpointModel(app, 2, new ColorRGBA(0, 1, 0, 0.4f)));
+        progress.setCheckpointModel(CheckpointProgress.GetDefaultCheckpointModel(app, 10, new ColorRGBA(0, 1, 0, 0.4f)));
         getStateManager().attach(progress);
 
         progressMenu = new CheckpointProgressUI(progress);
