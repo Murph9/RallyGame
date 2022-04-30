a98bbbda1674fd57b5c9741e3e2524a5cfe9fb92
Jake Murphy <murph9y@gmail.com>
2022-05-01 08:31:26 +1000

    add basic survival project stuff

---
+++ src/main/java/survival/SurvivalApp.java
@@ -0,0 +1,17 @@
+package survival;
+
+public class SurvivalApp {
+    
+    // Drive next x km to next checkpoint
+    // main problem is Fuel
+    // Drag gets worse as you drive (requiring more fuel)
+    // - time to next checkpoint might decreas slowly
+
+    // upgrade options are related to
+    // - improving general performance 
+    // - fuel economy
+    // - buying fuel
+
+    // After every 10x kms there are races which you must win, ie these are the boss milestones
+    // or maybe just a mandatory fuel sacrifice
+}
