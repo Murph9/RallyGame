c39032361e66723f849956ec90a54bf7c2ad9bdc
Jake Murphy <murph9y@gmail.com>
2022-05-21 16:48:06 +1000

    attempt to fix nre in waypoint class

--- src/main/java/rallygame/service/checkpoint/BasicWaypointProgress.java
+++ src/main/java/rallygame/service/checkpoint/BasicWaypointProgress.java
@@ -16,12 +16,14 @@ import com.jme3.bullet.util.CollisionShapeFactory;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
 
 import rallygame.car.ray.RayCarControl;
 import rallygame.effects.LoadModelWrapper;
 import rallygame.helper.Geo;
+import rallygame.helper.Log;
 import rallygame.service.GhostObjectCollisionListener;
 
 // should only have one checkpoint active
@@ -30,6 +32,8 @@ public class BasicWaypointProgress extends BaseAppState {
     private final BasicCheckpointListener listener;
     private final Queue<Vector3f> checkpointBuffer = new LinkedList<>();
     
+    private Node rootNode;
+
     private Checkpoint curCheckpoint;
     private int hitCount;
     private int totalHitCount;
@@ -41,11 +45,17 @@ public class BasicWaypointProgress extends BaseAppState {
     @Override
     protected void initialize(Application app) {
         this.listener.startListening(getState(BulletAppState.class).getPhysicsSpace());
+        
+        rootNode = new Node("Waypoint progress root node");
+        ((SimpleApplication)getApplication()).getRootNode().attachChild(rootNode);
     }
 
     @Override
     protected void cleanup(Application app) {
         this.listener.stopListening(getState(BulletAppState.class).getPhysicsSpace());
+
+        rootNode.removeFromParent();
+        rootNode = null;
     }
 
     public void addCheckpointAt(Vector3f pos) {
@@ -81,7 +91,7 @@ public class BasicWaypointProgress extends BaseAppState {
 
         curCheckpoint = new Checkpoint(totalHitCount, pos, ghost, baseSpat);
 
-        ((SimpleApplication)getApplication()).getRootNode().attachChild(baseSpat);
+        rootNode.attachChild(baseSpat);
         getState(BulletAppState.class).getPhysicsSpace().add(ghost);
     }
 
@@ -89,8 +99,12 @@ public class BasicWaypointProgress extends BaseAppState {
         hitCount++;
         totalHitCount++;
 
-        ((SimpleApplication)getApplication()).getRootNode().detachChild(curCheckpoint.visualModel);
-        getState(BulletAppState.class).getPhysicsSpace().remove(curCheckpoint.ghost);
+        if (curCheckpoint == null) {
+            Log.p("Very unusual that the checkpoint is null");
+        } else {
+            rootNode.detachChild(curCheckpoint.visualModel);
+            getState(BulletAppState.class).getPhysicsSpace().remove(curCheckpoint.ghost);
+        }
 
         curCheckpoint = null;
 
