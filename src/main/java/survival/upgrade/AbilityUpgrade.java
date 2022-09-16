e95d59a0d93162e264a4b9936b0eb693a5101648
Jake Murphy <murph9y@gmail.com>
2022-09-16 17:44:53 +1000

    greatly clean ability upgrade class

--- src/main/java/survival/upgrade/AbilityUpgrade.java
+++ src/main/java/survival/upgrade/AbilityUpgrade.java
@@ -10,45 +10,33 @@ import survival.ability.StopAbility;
 
 public class AbilityUpgrade extends Upgrade<List<Ability>> {
     private final Function<List<Upgrade<?>>, Boolean> applies;
+    private final boolean unique;
 
-    public AbilityUpgrade(boolean positive, String label, Consumer<List<Ability>> func, Function<List<Upgrade<?>>, Boolean> applies) {
+    public AbilityUpgrade(boolean positive, boolean unique, String label, Consumer<List<Ability>> func) {
+        this(positive, unique, label, func, null);
+    }
+    public AbilityUpgrade(boolean positive, boolean unique, String label, Consumer<List<Ability>> func, Function<List<Upgrade<?>>, Boolean> applies) {
         super(positive, label, func);
 
         this.applies = applies;
+        this.unique = unique;
     }
 
     @Override
     public boolean applies(List<Upgrade<?>> existing) {
+        if (unique) {
+            return !existing.contains(this);
+        }
+
+        if (applies == null)
+            return true;
         return applies.apply(existing);
     }
 
-    
-    public static Function<List<Upgrade<?>>, Boolean> AddExplodeAbilityFunc = (existing) -> {
-        for (var upgrade: existing) {
-            if (upgrade instanceof AbilityUpgrade) {
-                var explode = (AbilityUpgrade)upgrade;
-                if (explode.label.contains("Explode")) // TODO better
-                    return false;
-            }
-        }
-        return true;
-    };
-
-    public static Function<List<Upgrade<?>>, Boolean> AddStopAbilityFunc = (existing) -> {
-        for (var upgrade: existing) {
-            if (upgrade instanceof AbilityUpgrade) {
-                var explode = (AbilityUpgrade)upgrade;
-                if (explode.label.contains("Stop")) // TODO better
-                    return false;
-            }
-        }
-        return true;
-    };
-
-    public static Upgrade<List<Ability>> AddExplodeAbility = new AbilityUpgrade(true, "Add Explode Ability (Key: L ALT or Left Stick)", x -> x.add(new ExplodeAbility()), AbilityUpgrade.AddExplodeAbilityFunc);
-    public static Upgrade<List<Ability>> AddStopAbility = new AbilityUpgrade(true, "Add Stop Ability (Key: R ALT or R Bumper)", x -> x.add(new StopAbility()), AbilityUpgrade.AddStopAbilityFunc);
-    // public static Upgrade<List<Ability>> QuickerExplodeAbility = new AbilityUpgrade(true, "Improve Explode Ability", x -> x.ExplodeAbilityTimerMax *= 0.95f);
-    // public static Upgrade<List<Ability>> StrongerExplodeAbility = new Upgrade<>(true, "Increase Explode Ability Strength (5%)", x -> x.ExplodeAbilityStrength *= 1.05f);
+    public static Upgrade<List<Ability>> AddExplodeAbility = new AbilityUpgrade(true, true, "Add Explode Ability (Key: L ALT or Left Stick)", x -> x.add(new ExplodeAbility()));
+    public static Upgrade<List<Ability>> AddStopAbility = new AbilityUpgrade(true, true, "Add Stop Ability (Key: R ALT or R Bumper)", x -> x.add(new StopAbility()));
+    // public static Upgrade<List<Ability>> QuickerExplodeAbility = new AbilityUpgrade(true, false, "Improve Explode Ability", x -> x.ExplodeAbilityTimerMax *= 0.95f);
+    // public static Upgrade<List<Ability>> StrongerExplodeAbility = new Upgrade<>(true, false, "Increase Explode Ability Strength (5%)", x -> x.ExplodeAbilityStrength *= 1.05f);
 
     //TODO ability ideas:
     // freeze cubes (for some time)
